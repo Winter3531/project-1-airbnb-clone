2f95bec65b591143bc90077f0858777f5c8f747c
Winter3531 <winterdavid31@gmail.com>
2023-01-11 20:50:13 -0700

    auth-me-phase-2-cmplt

---
+++ backend/routes/index.js
@@ -0,0 +1,17 @@
+// backend/routes/index.js
+const express = require('express');
+const router = express.Router();
+const apiRouter = require('./api');
+
+
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
+});
+
+router.use('/api', apiRouter);
+
+module.exports = router;

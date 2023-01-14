904db8a5879aed2ae29ce1c75c0916fab7767fb0
Winter3531 <winterdavid31@gmail.com>
2023-01-14 13:27:27 -0700

    backend endpoint to get current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -32,4 +32,14 @@ router.delete('/', (_req, res) => {
     return res.json({message: 'Success'});
 });
 
+// Restore session User
+router.get('/', restoreUser, (req, res) => {
+    const {user} = req;
+    if (user) {
+        return res.json({
+            user: user.toSafeObject()
+        });
+    }else return res.json({ user: null});
+});
+
 module.exports = router;

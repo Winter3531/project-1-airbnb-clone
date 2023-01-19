af5d75e020236ee5da1b675eaa0d18cd7f8352bc
Winter3531 <winterdavid31@gmail.com>
2023-01-18 19:58:21 -0700

    user - spot associations

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -49,6 +49,10 @@ module.exports = (sequelize, DataTypes) => {
     }
     static associate(models) {
       // define association here
+      User.hasMany(
+        models.Spot,
+          {foreignKey: 'ownerId'}
+      );
     }
   }
   User.init({

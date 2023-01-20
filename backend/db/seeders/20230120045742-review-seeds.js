b769cd1516627465cf061ea19fda30ac0468a44d
Winter3531 <winterdavid31@gmail.com>
2023-01-19 22:26:57 -0700

    review seeds cmplt

---
+++ backend/db/seeders/20230120045742-review-seeds.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const { User, Spots } = require('../models');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Reviews', [
+      {
+        userId: 2,
+        spotId: 1,
+        review: 'Excellent location! Great pub near by!',
+        stars: 5,
+      },
+      {
+        userId: 3,
+        spotId: 4,
+        review: 'Quiet with lovely nearby views of the Beartooth and Absaroka Mtns.',
+        stars: 4,
+      },
+      {
+        userId: 1,
+        spotId: 3,
+        review: 'Great budget evening stay!',
+        stars: 3,
+      },
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete('Reviews', {
+      review: { [Op.in]: ['Excellent location! Great pub near by!', 'Quiet with lovely nearby views of the Beartooth and Absaroka Mtns.', 'Great budget evening stay!']}
+    });
+  }
+};

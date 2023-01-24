3c41b64011440f512c69d0adc1366af6fc7d41d1
Winter3531 <winterdavid31@gmail.com>
2023-01-23 21:46:19 -0700

    first review route cmplt

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const {Review, User, Spot, ReviewImage} = require('../../db/models');
+const {Review, User, Spot, ReviewImage, SpotImage} = require('../../db/models');
 const {sequelize} = require('../../db/models');
 const {requireAuth} = require('../../utils/auth');
 const {check} = require('express-validator');
@@ -26,9 +26,41 @@ router.get('/current', requireAuth, async (req, res) => {
 
     // convert the review and included data to a standard pojo
     const userReviewData = []
-    reviews.forEach()
+    reviews.forEach(data => {
+        userReviewData.push(data.toJSON());
+    })
 
-    res.json(reviews);
+    for(let i = 0; i < userReviewData.length; i++){
+        const review = userReviewData[i];
+
+        delete review.User.username;
+
+        delete review.Spot.description
+        delete review.Spot.createdAt
+        delete review.Spot.updatedAt
+
+        const preview = await SpotImage.findOne({
+            where: [
+                {
+                    spotId: review.Spot.id
+                }
+            ]
+        });
+        review.Spot.previewImage = preview.url;
+
+        review.ReviewImages.forEach(reviewImg => {
+            delete reviewImg.reviewId
+            delete reviewImg.createdAt
+            delete reviewImg.updatedAt
+        })
+    }
+
+
+    res.json({
+        Reviews: userReviewData
+    });
 })
+
+
 
 module.exports = router;

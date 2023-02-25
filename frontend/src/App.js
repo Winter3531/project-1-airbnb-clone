2c622cec420742f01a01f1c7b54b8495b8f11bf6
Winter3531 <winterdavid31@gmail.com>
2023-02-25 14:10:27 -0700

    pahse 1 & 2 cmplt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { Switch, Route } from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+
+import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from './components/SignupFormPage';
+import * as sessionActions from './store/session'
+
 function App() {
+
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restore()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
   return (
-    <h1>Hello from App</h1>
+    <Switch >
+      <Route path='/login' >
+        <LoginFormPage />
+      </Route>
+      <Route path='/signup' >
+        <SignupFormPage />
+      </Route>
+    </Switch>
   );
 }
 
